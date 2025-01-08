0d5fc7c60426b00a3af0bc169a0d371c992f4b95
Beatriz Castro <beacastro1008@gmail.com>
2025-01-08 19:47:06 +0000

    adicionando api inicial

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,4 @@
 
-async function fetchProfileData() {
-  const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json';
-  const fetching = await fetch(url)
-  return await fetching.json()
+async function fetchProfileData(url) {
+  return await (await fetch(url)).json()
 }
